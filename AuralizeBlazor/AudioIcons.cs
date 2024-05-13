9d2cc011156a52952b7ded9c1c3e6d6992ce60ab
fgilde <florian.gilde@sabio.de>
2024-05-13 07:41:01 +0200

    css

--- AuralizeBlazor/AudioIcons.cs
+++ AuralizeBlazor/AudioIcons.cs
@@ -35,6 +35,6 @@ public static class AudioIcons
     public const string Loop = "🔁";
     public const string LoopOne = "🔂";
     public const string Bars = "🎶";
-    public const string TrackList = "<svg class=\"mud-icon-root mud-svg-icon mud-icon-size-medium\" focusable=\"false\" viewBox=\"0 0 24 24\" aria-hidden=\"true\"><title>QueueMusic</title><!--!--><g><rect fill=\"none\" height=\"24\" width=\"24\"></rect></g><g><path d=\"M15,6H3v2h12V6z M15,10H3v2h12V10z M3,16h8v-2H3V16z M17,6v8.18C16.69,14.07,16.35,14,16,14c-1.66,0-3,1.34-3,3s1.34,3,3,3 s3-1.34,3-3V8h3V6H17z\"></path></g></svg>";
-    public const string Spectrum = "<svg class=\"mud-icon-root mud-svg-icon mud-icon-size-medium\" focusable=\"false\" viewBox=\"0 0 24 24\" aria-hidden=\"true\"><title>BarChart</title><!--!--><g><rect fill=\"none\" height=\"24\" width=\"24\"></rect></g><g><g><rect height=\"11\" width=\"4\" x=\"4\" y=\"9\"></rect><rect height=\"7\" width=\"4\" x=\"16\" y=\"13\"></rect><rect height=\"16\" width=\"4\" x=\"10\" y=\"4\"></rect></g></g></svg>";
+    public const string TrackList = "<path d=\"M15,6H3v2h12V6z M15,10H3v2h12V10z M3,16h8v-2H3V16z M17,6v8.18C16.69,14.07,16.35,14,16,14c-1.66,0-3,1.34-3,3s1.34,3,3,3 s3-1.34,3-3V8h3V6H17z\"></path>";
+    public const string Spectrum = "<rect fill=\"none\" height=\"24\" width=\"24\"></rect></g><g><g><rect height=\"11\" width=\"4\" x=\"4\" y=\"9\"></rect><rect height=\"7\" width=\"4\" x=\"16\" y=\"13\"></rect><rect height=\"16\" width=\"4\" x=\"10\" y=\"4\"></rect>";
 }
